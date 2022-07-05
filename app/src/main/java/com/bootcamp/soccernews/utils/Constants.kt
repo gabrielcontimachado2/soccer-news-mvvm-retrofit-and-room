70fb08c4073ddad7855e70e2f876ee381ac03b6c
Gabriel Conti <67682500+gabrielcontimachado2@users.noreply.github.com>
2022-07-05 06:33:39 -0300

    delete key

--- app/src/main/java/com/bootcamp/soccernews/utils/Constants.kt
+++ app/src/main/java/com/bootcamp/soccernews/utils/Constants.kt
@@ -3,7 +3,7 @@ package com.bootcamp.soccernews.utils
 class Constants {
     companion object {
         const val BASE_URL = "https://newsapi.org"
-        const val API_KEY = "1b8d2ac527b248c2a07e806579fedf53"
+        const val API_KEY = ""
         const val SEARCH_NEWS_DELAY = 500L
     }
 }
