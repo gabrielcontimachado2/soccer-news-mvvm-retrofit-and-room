febb3ff0ba25208f6b8fefda7f1bf63d49b09052
Gabriel Conti <67682500+gabrielcontimachado2@users.noreply.github.com>
2022-07-08 22:57:29 -0300

    WebView for news

--- app/src/main/java/com/bootcamp/soccernews/ui/fragments/pageNews/PageNewsFragment.kt
+++ app/src/main/java/com/bootcamp/soccernews/ui/fragments/pageNews/PageNewsFragment.kt
@@ -5,20 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.bootcamp.soccernews.R
+import android.webkit.WebViewClient
+import androidx.navigation.fragment.navArgs
+import com.bootcamp.soccernews.databinding.PageNewsFragmentBinding
 
 class PageNewsFragment : Fragment() {
 
+    private val args: PageNewsFragmentArgs by navArgs()
+
+    private var _binding: PageNewsFragmentBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.page_news_fragment, container, false)
+        _binding = PageNewsFragmentBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val news = args.news
+        var webViewNews = binding.webViewNews
+
+        webViewNews.apply {
+            webViewClient = WebViewClient()
+            news.url?.let { loadUrl(it) }
+        }
 
     }
 
